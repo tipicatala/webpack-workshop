17532eb24bd7ee047229178d46ffe443d4686bc5
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2021-10-17 11:24:35 +0300

    added plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack")
+
 module.exports = ({ mode }) => {
   return {
     mode,
     output: {
       filename: 'bundle.js'
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new webpack.ProgressPlugin()
+    ]
   }
 }
