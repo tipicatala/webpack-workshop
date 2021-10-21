e5a22f627cb8635ff59db42ea77b12602cb49460
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2021-10-21 12:08:03 +0300

    modified webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const webpack = require("webpack")
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
 
-module.exports = ({ mode }) => {
-  return {
-    mode,
-    output: {
-      filename: 'bundle.js'
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+  return merge(
+    {
+      mode,
+      output: {
+        filename: "bundle.js"
+      },
+      plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
     },
-    plugins: [
-      new HtmlWebpackPlugin(),
-      new webpack.ProgressPlugin()
-    ]
-  }
-}
+    modeConfig(mode)
+  );
+};
